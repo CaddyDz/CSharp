e2fed6ac6dbcd4dd485dd14dd664548bcdfc1092
Caddy Dz <tbitw31@gmail.com>
2016-12-28 19:47:59 -0800

    Exit Btn

--- Flask/Flask/Main.Designer.cs
+++ Flask/Flask/Main.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.HeaderPanel = new System.Windows.Forms.Panel();
+            this.MinimizeBtn = new System.Windows.Forms.Button();
+            this.MinMaxBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.HeaderPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HeaderPanel.Controls.Add(this.MinimizeBtn);
+            this.HeaderPanel.Controls.Add(this.MinMaxBtn);
+            this.HeaderPanel.Controls.Add(this.ExitBtn);
+            this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.Size = new System.Drawing.Size(1252, 42);
+            this.HeaderPanel.TabIndex = 0;
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.Location = new System.Drawing.Point(1149, 3);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(26, 23);
+            this.MinimizeBtn.TabIndex = 0;
+            this.MinimizeBtn.UseVisualStyleBackColor = true;
+            this.MinimizeBtn.Click += new System.EventHandler(this.MinimizeBtn_Click);
+            // 
+            // MinMaxBtn
+            // 
+            this.MinMaxBtn.Location = new System.Drawing.Point(1181, 3);
+            this.MinMaxBtn.Name = "MinMaxBtn";
+            this.MinMaxBtn.Size = new System.Drawing.Size(26, 23);
+            this.MinMaxBtn.TabIndex = 0;
+            this.MinMaxBtn.UseVisualStyleBackColor = true;
+            this.MinMaxBtn.Click += new System.EventHandler(this.MinMaxBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.BackgroundImage = global::Flask.Properties.Resources.ExitBtn;
+            this.ExitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ExitBtn.Location = new System.Drawing.Point(1213, 3);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(26, 23);
+            this.ExitBtn.TabIndex = 0;
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1448, 756);
+            this.ClientSize = new System.Drawing.Size(1252, 626);
+            this.Controls.Add(this.HeaderPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Flask";
+            this.HeaderPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel HeaderPanel;
+        private System.Windows.Forms.Button MinimizeBtn;
+        private System.Windows.Forms.Button MinMaxBtn;
+        private System.Windows.Forms.Button ExitBtn;
     }
 }
--- Flask/Flask/Main.cs
+++ Flask/Flask/Main.cs
@@ -16,5 +16,32 @@ namespace Flask
         {
             InitializeComponent();
         }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinMaxBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            } else if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void MinimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                MessageBox.Show("Test");
+                
+            }
+        }
     }
 }
