611502c47757007736fc3617fb3954be7cdd444f
Salim Djerbouh <github@caddydz.me>
2018-06-21 09:51:06 +0100

    Passing arguments to threads with lambda expressions
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- Program.cs
+++ Program.cs
@@ -11,65 +11,23 @@ namespace CSharpTutA
 
 				static void Main(string[] args)
 				{
-					BankAcct acct = new BankAcct(10);
-					Thread[] threads = new Thread[15];
+					Thread t = new Thread(() => CountTo(10));
+					t.Start();
 
-					Thread.CurrentThread.Name = "main";
+					new Thread(() => {
+						CountTo(5);
+						CountTo(6);
+					}).Start();
 
-					for (int i = 0; i < 15; i++)
-					{
-							Thread t = new Thread(new ThreadStart(acct.IssueWithdraw));
-							t.Name = i.ToString();
-							threads[i] = t;
-					}
-
-					for (int i = 0; i < 15; i++)
-					{
-							System.Console.WriteLine("Thread {0} Alive : {1}", threads[i].Name, threads[i].IsAlive);
-
-							threads[i].Start();
-
-							System.Console.WriteLine("Thread {0} Alive : {1}", threads[i].Name, threads[i].IsAlive);
-					}
-
-					System.Console.WriteLine("Current Priority {0}", Thread.CurrentThread.Priority);
-
-					System.Console.WriteLine("Thread {0} Ending", Thread.CurrentThread.Name);
-					
 					Console.ReadLine();
 				}
+
+				static void CountTo(int maxNum)
+				{
+					for (int i = 0; i <= maxNum; i++)
+					{
+							System.Console.WriteLine(i);
+					}
+				}
 		}
-}
-
-class BankAcct
-{
-	private Object acctLock = new object();
-	double Balance {set; get;}		
-
-	public BankAcct(double bal)
-	{
-			Balance = bal;
-	}
-
-	public double Withdraw(double amt)
-	{
-		if ((Balance - amt) < 0)
-		{
-				System.Console.WriteLine($"Sorrt ${Balance} in Account");
-				return Balance;
-		}
-		lock(acctLock)
-		{
-			if (Balance >= amt)
-			{
-					System.Console.WriteLine("Removed {0} and {1} left in Account", amt, (Balance - amt));
-					Balance -= amt;
-			}
-			return Balance;
-		}	
-	}
-	public void IssueWithdraw()
-	{
-		Withdraw(1);
-	}
 }
