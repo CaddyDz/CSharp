2d15b683266fd9b27c373877d88c3fb3ffa517d6
Salim Djerbouh <github@caddydz.me>
2018-06-21 22:37:13 +0100

    Tool bar
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- WpfTutorial/MainWindow.xaml.cs
+++ WpfTutorial/MainWindow.xaml.cs
@@ -1,6 +1,18 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace WpfTutorial
 {
@@ -50,6 +62,49 @@ namespace WpfTutorial
             menuFontCourier.IsChecked = false;
             menuFontTimes.IsChecked = false;
             txtBoxDoc.FontFamily = new FontFamily("Arial");
+        }
+
+        private bool comboFSClosed = true;
+
+        private void ComboFontSize_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            comboFSClosed = !comboBox.IsDropDownOpen;
+            ChangeTBFontSize();
+        }
+
+        private void ComboFontSize_DropDownClosed(object sender, System.EventArgs e)
+        {
+            if (comboFSClosed)
+            {
+                ChangeTBFontSize();
+            }
+            comboFSClosed = true;
+        }
+
+        private void ChangeTBFontSize()
+        {
+            string fontSize = comboFontSize.SelectedItem.ToString();
+
+            fontSize = fontSize.Substring(fontSize.Length - 2);
+
+            switch (fontSize)
+            {
+                case "10":
+                    txtBoxDoc.FontSize = 10;
+                    break;
+                case "12":
+                    txtBoxDoc.FontSize = 12;
+                    break;
+                case "14":
+                    txtBoxDoc.FontSize = 14;
+                    break;
+                case "16":
+                    txtBoxDoc.FontSize = 16;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
