5579f54a85bfb6b35b7448970404a1b454b9a6ed
Salim Djerbouh <github@caddydz.me>
2018-06-21 21:38:43 +0100

    Menu Bar
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- WpfTutorial/MainWindow.xaml.cs
+++ WpfTutorial/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
 
 namespace WpfTutorial
 {
@@ -24,9 +12,44 @@ namespace WpfTutorial
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            this.Title = "Hello World";
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.ShowDialog();
+        }
+
+        private void MenuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.ShowDialog();
+        }
+
+        private void MenuFontTimes_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontCourier.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Times New Roman");
+        }
+
+        private void MenuFontCourier_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontTimes.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Courier");
+        }
+
+        private void MenuFontArial_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontCourier.IsChecked = false;
+            menuFontTimes.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Arial");
         }
     }
 }
