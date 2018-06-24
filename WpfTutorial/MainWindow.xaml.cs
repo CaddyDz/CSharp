ef24cbbf72ef6ac13a44619342b8c1284896b74b
Salim Djerbouh <github@caddydz.me>
2018-06-24 21:05:26 +0100

    Document API
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- WpfTutorial/MainWindow.xaml.cs
+++ WpfTutorial/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -16,6 +18,7 @@ namespace WpfTutorial
         public MainWindow()
         {
             InitializeComponent();
+            GenerateDoc();
         }
 
         private void MyCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
@@ -119,6 +122,66 @@ namespace WpfTutorial
                 StrokeCollection sc = new StrokeCollection(fs);
                 this.DrawingCanvas.Strokes = sc;
             }
+        }
+
+        private void GenerateDoc()
+        {
+            FlowDocument doc = new FlowDocument();
+
+            Paragraph para = new Paragraph(new Run("Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next.First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs.She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it."));
+
+            doc.Blocks.Add(para);
+
+            para = new Paragraph(new Run("Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next.First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs.She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it."))
+            {
+                FontSize = 14,
+                FontStyle = FontStyles.Italic,
+                Foreground = Brushes.Green
+            };
+            doc.Blocks.Add(para);
+            fdScrollViewer.Document = doc;
+        }
+
+        private void RichTB_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            if (rtb == null) return;
+            ContextMenu contextMenu = rtb.ContextMenu;
+            contextMenu.PlacementTarget = rtb;
+            contextMenu.Placement = PlacementMode.RelativePoint;
+            TextPointer position = rtb.Selection.End;
+
+            if (position == null) return;
+            Rect positionRect = position.GetCharacterRect(LogicalDirection.Forward);
+            contextMenu.HorizontalOffset = positionRect.X;
+            contextMenu.VerticalOffset = positionRect.Y;
+
+            contextMenu.IsOpen = true;
+            e.Handled = true;
+        }
+
+        void SaveRTBContent(object sender, RoutedEventArgs e)
+        {
+            TextRange range = new TextRange(RichTB.Document.ContentStart, RichTB.Document.ContentEnd);
+            FileStream fStream = new FileStream(@"C:\Users\Salim\C#Data\text.xaml", FileMode.Create);
+
+            range.Save(fStream, DataFormats.XamlPackage);
+            fStream.Close();
+        }
+
+        void OpenRTBContent(object sender, RoutedEventArgs e)
+        {
+            TextRange range;
+            FileStream fStream;
+
+            if(File.Exists(@"C:\Users\Salim\C#Data\text.xaml"))
+            {
+                range = new TextRange(RichTB.Document.ContentStart, RichTB.Document.ContentEnd);
+                fStream = new FileStream(@"C:\Users\Salim\C#Data\text.xaml", FileMode.OpenOrCreate);
+                range.Load(fStream, DataFormats.XamlPackage);
+                fStream.Close();
+            }
+            
         }
     }
 }
